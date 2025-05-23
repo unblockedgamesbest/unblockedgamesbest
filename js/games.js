4b4c1743b2e423799b9a7557f065af7a4ab58f8b
unblockedgamesbest <212731308+unblockedgamesbest@users.noreply.github.com>
2025-05-23 13:55:06 +0800

    wheelie bike

--- js/games.js
+++ js/games.js
@@ -1,6 +1,7 @@
 const games = [
     { name: "Crazy Cattle 3d", image: "image/crazycattle.png", link: "/crazycattle3d.html"},
     { name: "Speed Stars", image: "image/speedstars.png", link: "/speedstars.html"},
+    { name: "Wheelie Bike", image: "image/wheeliebike.png", link: "/wheeliebike.html"},
 ];
 
 /*createGameCard*/
