4ef8c473138cd3e1e4b93641f84763d9007d42cd
unblockedgamesbest <212731308+unblockedgamesbest@users.noreply.github.com>
2025-05-23 14:38:16 +0800

    drift hunters

--- js/games.js
+++ js/games.js
@@ -2,6 +2,7 @@ const games = [
     { name: "Crazy Cattle 3d", image: "image/crazycattle.png", link: "/crazycattle3d.html"},
     { name: "Speed Stars", image: "image/speedstars.png", link: "/speedstars.html"},
     { name: "Wheelie Bike", image: "image/wheeliebike.png", link: "/wheeliebike.html"},
+    { name: "Drift Hunters", image: "image/drifthunters.png", link: "/drifthunters.html"},
 ];
 
 /*createGameCard*/
